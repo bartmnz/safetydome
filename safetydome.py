1dd71f2cb14e7414be9f5da8dda3daa2b1fe8455
Sean Bartholomew <bartmnz@gmail.com>
2016-06-17 19:32:06 -0400

    pep8

--- safetydome.py
+++ safetydome.py
@@ -14,10 +14,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/combatant/<combatant_id>')
-def combatant(combatant_id = None):
+def combatant(combatant_id=None):
     con, cursor = connect()
-    
+
     class combatant():
         def __init__(self, args):
             self.name = args[0]
@@ -55,7 +56,6 @@ def combatant(combatant_id = None):
         'WHERE ' +\
         " combatant.id = '" + str(combatant_id) + "' AND " +\
         ' species.id = combatant.species_id;'
-    
     cursor.execute(querry)
     results = cursor.fetchall()
     output = []
@@ -65,12 +65,12 @@ def combatant(combatant_id = None):
     con.commit()
     con.close()
     return render_template('combatant.html', combatants=output)
-        
+
 
 @app.route('/results/')
 def results():
     con, cursor = connect()
-    
+
     class result():
         def __init__(self, rank, args):
             self.rank = rank
@@ -83,7 +83,7 @@ def results():
         'count(fight.combatant_one) ' +\
         'FROM ' +\
         ' (SELECT ' +\
-        '   fight.combatant_one '+\
+        '   fight.combatant_one ' +\
         '  FROM ' +\
         '   public.fight ' +\
         '  WHERE ' +\
@@ -92,7 +92,7 @@ def results():
         '  SELECT ' +\
         '   fight.combatant_two ' +\
         '  FROM ' +\
-        '   public.fight '+\
+        '   public.fight ' +\
         '  WHERE ' +\
         "   fight.winner = 'Two') as fight, " +\
         ' combatant ' +\
@@ -102,12 +102,11 @@ def results():
         ' combatant.id ' +\
         'ORDER BY ' +\
         ' count(fight.combatant_one) DESC;'
-    
+
     cursor.execute(querry)
     results = cursor.fetchall()
     output = []
     rank = 1
-    print( results )
     for data in results:
         info = result(str(rank), data)
         output.append(info)
@@ -116,15 +115,13 @@ def results():
     con.commit()
     con.close()
     return render_template('results.html', combatants=output)
-    
-    
-#    TODO
+
 
 @app.route('/battle/<id1>-<id2>')
 @app.route('/battle/<battle_id>')
-def battles(battle_id = None, id1 = None, id2 = None):
+def battles(battle_id=None, id1=None, id2=None):
     con, cursor = connect()
-    
+
     class fight():
         def __init__(self, args):
             self.one_id = args[1]
@@ -139,7 +136,7 @@ def battles(battle_id = None, id1 = None, id2 = None):
             self.fight_id = args[5]
             self.start = args[6]
             self.end = args[7]
-    if( battle_id ):
+    if(battle_id):
         querry = 'SELECT ' +\
             'fight.winner, ' +\
             'combatant_one, ' +\
@@ -152,10 +149,10 @@ def battles(battle_id = None, id1 = None, id2 = None):
             'fight.start, ' +\
             'fight.finish ' +\
             'FROM ' +\
-            ' fight '+\
+            ' fight ' +\
             'WHERE ' +\
             ' fight.id = ' + str(battle_id) + ';'
-    elif( id1 and id2 ):
+    elif(id1 and id2):
         querry = 'SELECT ' +\
             'fight.winner, ' +\
             'combatant_one, ' +\
@@ -168,7 +165,7 @@ def battles(battle_id = None, id1 = None, id2 = None):
             'fight.start, ' +\
             'fight.finish ' +\
             'FROM ' +\
-            ' fight '+\
+            ' fight ' +\
             'WHERE ' +\
             ' combatant_one = ' + str(id1) + ' AND ' +\
             ' combatant_two = ' + str(id2) + ';'
@@ -176,15 +173,15 @@ def battles(battle_id = None, id1 = None, id2 = None):
     results = cursor.fetchall()
     info = fight(results[0])
     fights = [info]
-            
     con.commit()
     con.close()
     return render_template('battle_id.html', fights=fights)
 
+
 @app.route('/battle')
-def battle(name = None):
+def battle(name=None):
     con, cursor = connect()
-    
+
     class fight():
         def __init__(self, args):
             self.one_id = args[1]
@@ -211,7 +208,6 @@ def battle(name = None):
     cursor.execute(querry)
     results = cursor.fetchall()
     battle_list = []
-    
     for battle in results:
         new_fight = fight(battle)
         battle_list.append(new_fight)
@@ -224,7 +220,6 @@ def battle(name = None):
 @app.route('/combatant/')
 def combatants():
     con, cursor = connect()
-    
 
     class combatant():
         def __init__(self, args):
@@ -243,8 +238,6 @@ def combatants():
     cursor.execute(querry)
     results = cursor.fetchall()
     combatant_list = []
-    print( results)
-    
     for guy in results:
         new_guy = combatant(guy)
         combatant_list.append(new_guy)
