8c09cde3793659adb3b1129af7bed8852ae2c04c
Sean Bartholomew <bartmnz@gmail.com>
2016-06-17 23:25:51 -0400

    added error checking

--- safetydome.py
+++ safetydome.py
@@ -17,54 +17,58 @@ def index():
 
 @app.route('/combatant/<int:combatant_id>')
 def combatant(combatant_id=None):
-    con, cursor = connect()
-
-    class combatant():
-        def __init__(self, args):
-            self.name = args[0]
-            self.species = args[1]
-            self.attack = args[2]
-            self.defense = args[3]
-            self.health = args[4]
-    querry = 'SELECT ' +\
-        'combatant.name, ' +\
-        'species.name, ' +\
-        'combatant.plus_atk + ' +\
-        ' (SELECT ' +\
-        '  species.base_atk ' +\
-        ' FROM ' +\
-        '  species ' +\
-        ' WHERE ' +\
-        '  species.id = combatant.species_id), ' +\
-        'combatant.plus_dfn + ' +\
-        ' (SELECT ' +\
-        '  species.base_dfn ' +\
-        ' FROM ' +\
-        '  species ' +\
-        ' WHERE ' +\
-        '  species.id = combatant.species_id), ' +\
-        'combatant.plus_hp + ' +\
-        ' (SELECT ' +\
-        '  species.base_hp ' +\
-        ' FROM ' +\
-        '  species ' +\
-        ' WHERE ' +\
-        '  species.id = combatant.species_id) ' +\
-        'FROM ' +\
-        ' combatant, ' +\
-        ' species ' +\
-        'WHERE ' +\
-        " combatant.id = '" + str(combatant_id) + "' AND " +\
-        ' species.id = combatant.species_id;'
-    cursor.execute(querry) # use flask type conversion for no can has SQL injection
-    results = cursor.fetchall()
     output = []
-    for data in results:
-        info = combatant(data)
-        output.append(info)
-    con.commit()
-    con.close()
-    return render_template('combatant.html', combatants=output)
+    try:
+        con, cursor = connect()
+
+        class combatant():
+            def __init__(self, args):
+                self.name = args[0]
+                self.species = args[1]
+                self.attack = args[2]
+                self.defense = args[3]
+                self.health = args[4]
+        querry = 'SELECT ' +\
+            'combatant.name, ' +\
+            'species.name, ' +\
+            'combatant.plus_atk + ' +\
+            ' (SELECT ' +\
+            '  species.base_atk ' +\
+            ' FROM ' +\
+            '  species ' +\
+            ' WHERE ' +\
+            '  species.id = combatant.species_id), ' +\
+            'combatant.plus_dfn + ' +\
+            ' (SELECT ' +\
+            '  species.base_dfn ' +\
+            ' FROM ' +\
+            '  species ' +\
+            ' WHERE ' +\
+            '  species.id = combatant.species_id), ' +\
+            'combatant.plus_hp + ' +\
+            ' (SELECT ' +\
+            '  species.base_hp ' +\
+            ' FROM ' +\
+            '  species ' +\
+            ' WHERE ' +\
+            '  species.id = combatant.species_id) ' +\
+            'FROM ' +\
+            ' combatant, ' +\
+            ' species ' +\
+            'WHERE ' +\
+            " combatant.id = '" + str(combatant_id) + "' AND " +\
+            ' species.id = combatant.species_id;'
+        cursor.execute(querry)  # flask type coversion avoid SQL injection
+        results = cursor.fetchall()
+        if results:
+            for data in results:
+                info = combatant(data)
+                output.append(info)
+        con.commit()
+        con.close()
+        return render_template('combatant.html', combatants=output)
+    except:
+        return "ERROR happened"
 
 
 @app.route('/results/')
@@ -120,132 +124,144 @@ def results():
 @app.route('/battle/<int:id1>-<int:id2>')
 @app.route('/battle/<int:battle_id>')
 def battles(battle_id=None, id1=None, id2=None):
-    con, cursor = connect()
+    try:
+        con, cursor = connect()
 
-    class fight():
-        def __init__(self, args):
-            self.one_id = args[1]
-            self.two_id = args[2]
-            self.one_name = args[3]
-            self.two_name = args[4]
-            self.winner = 'Tie'
-            if str(args[0]) == 'One':
-                self.winner = self.one_name
-            elif str(args[0]) == 'Two':
-                self.winner = self.two_name
-            self.fight_id = args[5]
-            self.start = args[6]
-            self.end = args[7]
-    if(battle_id):
-        querry = 'SELECT ' +\
-            'fight.winner, ' +\
-            'combatant_one, ' +\
-            'combatant_two, ' +\
-            '(SELECT name FROM combatant ' +\
-            ' WHERE fight.combatant_one = combatant.id), ' +\
-            '(SELECT name FROM combatant ' +\
-            ' WHERE fight.combatant_two = combatant.id), ' +\
-            'fight.id, ' +\
-            'fight.start, ' +\
-            'fight.finish ' +\
-            'FROM ' +\
-            ' fight ' +\
-            'WHERE ' +\
-            " fight.id = '" + str(battle_id) + "';"
-        cursor.execute(querry) #no SQL injection
-    elif(id1 and id2):
-        querry = 'SELECT ' +\
-            'fight.winner, ' +\
-            'combatant_one, ' +\
-            'combatant_two, ' +\
-            '(SELECT name FROM combatant ' +\
-            ' WHERE fight.combatant_one = combatant.id), ' +\
-            '(SELECT name FROM combatant ' +\
-            ' WHERE fight.combatant_two = combatant.id), ' +\
-            'fight.id, ' +\
-            'fight.start, ' +\
-            'fight.finish ' +\
-            'FROM ' +\
-            ' fight ' +\
-            'WHERE ' +\
-            " combatant_one = '" + str(int1) + "' AND " +\
-            " combatant_two = '" + str(int2) + "';"
-        cursor.execute(querry, id1, id2) #no SQL injection
-    results = cursor.fetchall()
-    info = fight(results[0])
-    fights = [info]
-    con.commit()
-    con.close()
-    return render_template('battle_id.html', fights=fights)
+        class fight():
+            def __init__(self, args):
+                self.one_id = args[1]
+                self.two_id = args[2]
+                self.one_name = args[3]
+                self.two_name = args[4]
+                self.winner = 'Tie'
+                if str(args[0]) == 'One':
+                    self.winner = self.one_name
+                elif str(args[0]) == 'Two':
+                    self.winner = self.two_name
+                self.fight_id = args[5]
+                self.start = args[6]
+                self.end = args[7]
+        if(battle_id):
+            querry = 'SELECT ' +\
+                'fight.winner, ' +\
+                'combatant_one, ' +\
+                'combatant_two, ' +\
+                '(SELECT name FROM combatant ' +\
+                ' WHERE fight.combatant_one = combatant.id), ' +\
+                '(SELECT name FROM combatant ' +\
+                ' WHERE fight.combatant_two = combatant.id), ' +\
+                'fight.id, ' +\
+                'fight.start, ' +\
+                'fight.finish ' +\
+                'FROM ' +\
+                ' fight ' +\
+                'WHERE ' +\
+                " fight.id = '" + str(battle_id) + "';"
+            cursor.execute(querry)  # no SQL injection
+        elif(id1 and id2):
+            querry = 'SELECT ' +\
+                'fight.winner, ' +\
+                'combatant_one, ' +\
+                'combatant_two, ' +\
+                '(SELECT name FROM combatant ' +\
+                ' WHERE fight.combatant_one = combatant.id), ' +\
+                '(SELECT name FROM combatant ' +\
+                ' WHERE fight.combatant_two = combatant.id), ' +\
+                'fight.id, ' +\
+                'fight.start, ' +\
+                'fight.finish ' +\
+                'FROM ' +\
+                ' fight ' +\
+                'WHERE ' +\
+                " combatant_one = '" + str(id1) + "' AND " +\
+                " combatant_two = '" + str(id2) + "';"
+            cursor.execute(querry)  # no SQL injection
+        results = cursor.fetchall()
+        fights = []
+        if results:
+            info = fight(results[0])
+            fights.append(info)
+        con.commit()
+        con.close()
+        return render_template('battle_id.html', fights=fights)
+    except:
+        return "ERROR happened"
 
 
 @app.route('/battle')
 def battle(name=None):
-    con, cursor = connect()
+    try:
+        con, cursor = connect()
 
-    class fight():
-        def __init__(self, args):
-            self.one_id = args[1]
-            self.two_id = args[2]
-            self.one_name = args[3]
-            self.two_name = args[4]
-            self.winner = 'Tie'
-            if str(args[0]) == 'One':
-                self.winner = self.one_name
-            elif str(args[0]) == 'Two':
-                self.winner = self.two_name
-            self.fight_id = args[5]
-    querry = 'SELECT ' +\
-        'fight.winner, ' +\
-        'combatant_one, ' +\
-        'combatant_two, ' +\
-        '(SELECT name FROM combatant ' +\
-        ' WHERE fight.combatant_one = combatant.id), ' +\
-        '(SELECT name FROM combatant ' +\
-        ' WHERE fight.combatant_two = combatant.id), ' +\
-        'fight.id ' +\
-        'FROM ' +\
-        ' fight;'
-    cursor.execute(querry)
-    results = cursor.fetchall()
-    battle_list = []
-    for battle in results:
-        new_fight = fight(battle)
-        battle_list.append(new_fight)
-    con.commit()
-    con.close()
-    return render_template('battle.html', fights=battle_list)
-#    TODO
+        class fight():
+            def __init__(self, args):
+                self.one_id = args[1]
+                self.two_id = args[2]
+                self.one_name = args[3]
+                self.two_name = args[4]
+                self.winner = 'Tie'
+                if str(args[0]) == 'One':
+                    self.winner = self.one_name
+                elif str(args[0]) == 'Two':
+                    self.winner = self.two_name
+                self.fight_id = args[5]
+        querry = 'SELECT ' +\
+            'fight.winner, ' +\
+            'combatant_one, ' +\
+            'combatant_two, ' +\
+            '(SELECT name FROM combatant ' +\
+            ' WHERE fight.combatant_one = combatant.id), ' +\
+            '(SELECT name FROM combatant ' +\
+            ' WHERE fight.combatant_two = combatant.id), ' +\
+            'fight.id ' +\
+            'FROM ' +\
+            ' fight;'
+        cursor.execute(querry)
+        results = cursor.fetchall()
+        battle_list = []
+        if results:
+            for battle in results:
+                new_fight = fight(battle)
+                battle_list.append(new_fight)
+        con.commit()
+        con.close()
+        return render_template('battle.html', fights=battle_list)
+    except:
+        return "ERROR happened"
 
 
 @app.route('/combatant/')
 def combatants():
-    con, cursor = connect()
+    try:
+        con, cursor = connect()
 
-    class combatant():
-        def __init__(self, args):
-            self.id = args[0]
-            self.name = args[1]
-            self.species = args[2]
-    querry = 'SELECT ' +\
-        'combatant.id, combatant.name, species.name ' +\
-        'FROM ' +\
-        ' combatant, ' +\
-        ' species ' +\
-        'WHERE ' +\
-        ' combatant.species_id = species.id ' +\
-        'ORDER BY ' +\
-        ' combatant.name;'
-    cursor.execute(querry)
-    results = cursor.fetchall()
-    combatant_list = []
-    for guy in results:
-        new_guy = combatant(guy)
-        combatant_list.append(new_guy)
-#    print(combatant_list)
-    con.commit()
-    con.close()
-    return render_template('combatants.html', combatants=combatant_list)
+        class combatant():
+            def __init__(self, args):
+                self.id = args[0]
+                self.name = args[1]
+                self.species = args[2]
+        querry = 'SELECT ' +\
+            'combatant.id, combatant.name, species.name ' +\
+            'FROM ' +\
+            ' combatant, ' +\
+            ' species ' +\
+            'WHERE ' +\
+            ' combatant.species_id = species.id ' +\
+            'ORDER BY ' +\
+            ' combatant.name;'
+        cursor.execute(querry)
+        results = cursor.fetchall()
+        combatant_list = []
+        if results:
+            for guy in results:
+                new_guy = combatant(guy)
+                combatant_list.append(new_guy)
+    #    print(combatant_list)
+        con.commit()
+        con.close()
+        return render_template('combatants.html', combatants=combatant_list)
+    except:
+        "ERROR happened"
 
 
 def connect():
